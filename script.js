4a2154a243ffc5d2ac1ee2b22c87ee041ba2d720
Arif Rahman <xossarif@gmail.com>
2025-03-01 20:16:24 +0600

    done final version

--- script.js
+++ script.js
@@ -23,3 +23,68 @@ function randomColor() {
 document.getElementById("color-btn").addEventListener("click", function () {
 	document.body.style.backgroundColor = randomColor();
 });
+
+// task completion
+let taskAssigned = 6;
+let taskCompleted = 23;
+const taskCompletedEl = document.getElementById("task-completed");
+const taskAssignedEl = document.getElementById("task-assigned");
+const taskCompletedItems = document.querySelectorAll(".task-completed");
+const activityLogContainer = document.getElementById("activity-log-message-container");
+
+function getCurrentTime() {
+	const now = new Date();
+	return now.toLocaleTimeString({ hour: "2-digit", minute: "2-digit", second: "2-digit", hour12: true });
+}
+
+if (taskCompletedItems) {
+	for (let task of taskCompletedItems) {
+		task.addEventListener("click", function (event) {
+			if (!this.disabled) {
+				alert("Board Updated Successfully");
+				if (taskAssigned > 0) {
+					taskAssigned--;
+					taskCompleted++;
+				}
+
+				taskAssignedEl.textContent = taskAssigned;
+				taskCompletedEl.textContent = taskCompleted;
+
+				let taskTitle = this.closest(".task-col").querySelector(".task-title").textContent;
+
+				const activityLog = document.createElement("p");
+				activityLog.classList.add("bg-light-blue", "p-2.5", "rounded-lg", "mb-7");
+				activityLog.textContent = `You have completed the task ${taskTitle} at ${getCurrentTime()}`;
+				activityLogContainer.appendChild(activityLog);
+
+				this.disabled = true;
+
+				if (taskAssigned === 0) {
+					alert("Congrats!!! You have completed all the current task");
+				}
+			}
+		});
+	}
+}
+
+// clear task log
+const clearTaskLogBtn = document.getElementById("clear-task-log-btn");
+if (clearTaskLogBtn) {
+	clearTaskLogBtn.addEventListener("click", function () {
+		activityLogContainer.innerHTML = "";
+	});
+}
+
+// to show current date and day
+function todaysDate() {
+	const today = new Date();
+	let dayFormat = { weekday: "short" };
+	let dayName = today.toLocaleDateString("en-US", dayFormat);
+
+	let dateFormat = { month: "short", day: "numeric", year: "numeric" };
+	let fullDate = today.toLocaleDateString("en-US", dateFormat);
+
+	document.getElementById("day").innerText = dayName;
+	document.getElementById("fullDate").innerText = fullDate;
+}
+todaysDate();
